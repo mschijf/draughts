516d0c8505f08ba64e4892790076961a43cc3516
mschijf <mschijf@gmail.com>
2022-09-11 08:18:41 +0200

    Adding test and perft

--- board/move_test.go
+++ board/move_test.go
@@ -6,11 +6,30 @@ import (
 	"time"
 )
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// depth   1  :     0.000015 ms -->              9
+// depth   2  :     0.000003 ms -->             81
+// depth   3  :     0.000038 ms -->            658
+// depth   4  :     0.000258 ms -->           4265
+// depth   5  :     0.001789 ms -->          27117
+// depth   6  :     0.008484 ms -->         167140
+// depth   7  :     0.058329 ms -->        1049442
+// depth   8  :     0.310662 ms -->        6483961
+// depth   9  :     1.938674 ms -->       41022423
+// depth  10  :    12.458864 ms -->      258895763
+//
+// appr. 20.780.045 nodes/second
+//
+// see also https://damforum.nl/bb3/viewtopic.php?t=2308
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 func (bitBoard *BitBoard) perft(depth int, colorToMove int) int64 {
 	if depth == 0 {
 		return 1
 	}
-	
+
 	moveList := bitBoard.GenerateMoves(colorToMove)
 	if len(moveList) == 0 {
 		return 1
@@ -25,36 +44,35 @@ func (bitBoard *BitBoard) perft(depth int, colorToMove int) int64 {
 	return nodeCount
 }
 
-// func Test_tBitBoard_perft(t *testing.T) {
-// 	hb := InitStartBoard()
+func Test_bitBoard_perft(t *testing.T) {
+	hb := InitStartBoard()
 
-// 	tables := []struct {
-// 		x int
-// 		n int64
-// 	}{
-// 		{1, 4},
-// 		{2, 12},
-// 		{3, 56},
-// 		{4, 244},
-// 		{5, 1396},
-// 		{6, 8200},
-// 		{7, 55092},
-// 		{8, 390216},
-// 		{9, 3005288},
-// 		{10, 24571284},
-// 	}
+	tables := []struct {
+		x int
+		n int64
+	}{
+		{1, 9},
+		{2, 81},
+		{3, 658},
+		{4, 4265},
+		{5, 27117},
+		{6, 167140},
+		{7, 1049442},
+		{8, 6483961},
+		{9, 41022423},
+		{10, 258895763},
+	}
 
-// 	for _, table := range tables {
-// 		nodeCount := hb.bitBoard.perft(table.x, hb.colorToMove, false)
-// 		if nodeCount != table.n {
-// 			t.Errorf("Perft of %d was incorrect, got: %d, want: %d.", table.x, nodeCount, table.n)
-// 		}
-// 	}
-// }
+	for _, table := range tables {
+		nodeCount := hb.bitBoard.perft(table.x, hb.colorToMove)
+		if nodeCount != table.n {
+			t.Errorf("Perft of %d was incorrect, got: %d, want: %d.", table.x, nodeCount, table.n)
+		}
+	}
+}
 
 func Test_bitBoard_perft_print(t *testing.T) {
 	var hb = InitStartBoard()
-
 	for i := 1; i < 12; i++ {
 		currentTime := time.Now()
 		result := hb.bitBoard.perft(i, hb.colorToMove)
